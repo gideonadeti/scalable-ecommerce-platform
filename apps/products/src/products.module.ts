c54f1f03d07af99f49ecf9ce82c9ae5e3fdbe045
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-23 21:29:16 +0000

    feat(prisma): Add PrismaService and its test suite

--- apps/products/src/products.module.ts
+++ apps/products/src/products.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'apps/products/.env',
+    }),
+  ],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, PrismaService],
 })
 export class ProductsModule {}

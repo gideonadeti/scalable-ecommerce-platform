7f5c5542078027e1045a394106a95b717d32c094
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-23 13:19:36 +0000

    feat(api-gateway): Add ConfigModule for environment configuration

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'apps/api-gateway/.env',
+    }),
+  ],
 })
 export class ApiGatewayModule {}

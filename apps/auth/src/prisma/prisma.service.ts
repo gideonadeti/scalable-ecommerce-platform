85b573e74c55adf559c8f9c582d6b2eb4be944c0
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-23 03:36:11 +0000

    feat(prisma): Add ConfigService to PrismaService constructor to configure database url

--- apps/auth/src/prisma/prisma.service.ts
+++ apps/auth/src/prisma/prisma.service.ts
@@ -1,12 +1,18 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { PrismaClient } from 'apps/auth/generated/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
+  constructor(configService: ConfigService) {
     super({
       errorFormat: 'pretty',
+      datasources: {
+        db: {
+          url: configService.get('DATABASE_URL'),
+        },
+      },
     });
   }
 

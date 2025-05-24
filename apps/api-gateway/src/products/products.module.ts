5238f8dca6179f32798facbdd5749f63a362573f
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-24 05:53:36 +0000

    feat(products): Configure microservice with RabbitMQ transport
    Set up the Products module to use RabbitMQ for messaging. This includes
    registering the ClientsModule asynchronously with the necessary
    configuration for the message broker.

--- apps/api-gateway/src/products/products.module.ts
+++ apps/api-gateway/src/products/products.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 
 @Module({
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        imports: [ConfigModule],
+        name: 'PRODUCTS_SERVICE',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('MESSAGE_BROKER_URL') as string],
+            queue: 'products_queue',
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { AsyncMicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+
 import { ProductsModule } from './products.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ProductsModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<AsyncMicroserviceOptions>(
+    ProductsModule,
+    {
+      useFactory: (configService: ConfigService) => ({
+        transport: Transport.RMQ,
+        options: {
+          urls: [configService.get<string>('MESSAGE_BROKER_URL') as string],
+          queue: 'products_queue',
+        },
+      }),
+      inject: [ConfigService],
+    },
+  );
+
+  await app.listen();
 }
+
 bootstrap();

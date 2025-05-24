825570f4f087139fac42de82414b467425b33759
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-24 05:54:19 +0000

    feat(products): Add CreateProductDto with validation rules

--- apps/api-gateway/src/products/dto/create-product.dto.ts
+++ apps/api-gateway/src/products/dto/create-product.dto.ts
@@ -1,1 +1,29 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+
+export class CreateProductDto {
+  /**
+   * Product's name
+   * @example 'Laptop'
+   */
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  /**
+   * Product's price
+   * @example 99.99
+   */
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0)
+  price: number;
+
+  /**
+   * Product's quantity
+   * @example 10
+   */
+  @IsInt()
+  @IsNotEmpty()
+  @Min(0)
+  quantity: number;
+}

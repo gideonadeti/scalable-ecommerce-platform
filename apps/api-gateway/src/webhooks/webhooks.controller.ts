c36e443a74ad51002cf17171d39d5eec8c528a1d
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-26 13:22:25 +0000

    feat(webhooks): Implement Stripe webhook handling for successful checkouts
    Add functionality to handle successful checkout events from Stripe.
    This includes processing the webhook signature and emitting an event
    to the payment service for further processing.

--- apps/api-gateway/src/webhooks/webhooks.controller.ts
+++ apps/api-gateway/src/webhooks/webhooks.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { Controller, Headers, Post, RawBodyRequest, Req } from '@nestjs/common';
+
+import { WebhooksService } from './webhooks.service';
 
 @Controller('webhooks')
-export class WebhooksController {}
+export class WebhooksController {
+  constructor(private readonly webhooksService: WebhooksService) {}
+
+  @Post('stripe')
+  handleSuccessfulCheckout(
+    @Req() req: RawBodyRequest<Request>,
+    @Headers('stripe-signature') sig: string,
+  ) {
+    return this.webhooksService.handleSuccessfulCheckout(req, sig);
+  }
+}
--- apps/api-gateway/src/webhooks/webhooks.service.ts
+++ apps/api-gateway/src/webhooks/webhooks.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
+import { ConfigService } from '@nestjs/config';
+import { Request, Response } from 'express';
+import { ClientProxy } from '@nestjs/microservices';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  RawBodyRequest,
+} from '@nestjs/common';
+
+import { MicroserviceError } from '../interfaces/microservice-error/microservice-error.interface';
 
 @Injectable()
-export class WebhooksService {}
+export class WebhooksService {
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject('PAYMENT_SERVICE') private paymentClient: ClientProxy,
+  ) {}
+
+  private logger = new Logger(WebhooksService.name);
+
+  private handleError(error: MicroserviceError, action: string) {
+    this.logger.error(`Failed to ${action}`, error);
+
+    throw new InternalServerErrorException(`Failed to ${action}`);
+  }
+
+  handleSuccessfulCheckout(req: RawBodyRequest<Request>, sig: string) {
+    const webhookSecret = this.configService.get<string>(
+      'STRIPE_WEBHOOK_SIGNING_SECRET',
+    );
+    const stripeSecretKey = this.configService.get<string>('STRIPE_SECRET_KEY');
+    const stripe = new Stripe(stripeSecretKey as string);
+
+    try {
+      const event = stripe.webhooks.constructEvent(
+        req.rawBody as Buffer,
+        sig,
+        webhookSecret as string,
+      );
+
+      this.logger.log(`Stripe event received: ${event.type}`);
+
+      if (event.type === 'checkout.session.completed') {
+        const session = event.data.object;
+
+        this.paymentClient.emit('handle-successful-checkout', session);
+      }
+
+      return { received: true };
+    } catch (error) {
+      this.handleError(
+        error as MicroserviceError,
+        'handle successful checkout',
+      );
+    }
+  }
+}

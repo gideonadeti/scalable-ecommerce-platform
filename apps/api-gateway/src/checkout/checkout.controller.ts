868ea0403a754566fa062437818b972982467edb
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-26 05:25:01 +0000

    feat(checkout): Implement CheckoutController and CheckoutService
    Add CheckoutController with JWT authentication and a checkout method
    that interacts with the payment microservice. Enhance CheckoutService
    to handle errors and manage the checkout process.

--- apps/api-gateway/src/checkout/checkout.controller.ts
+++ apps/api-gateway/src/checkout/checkout.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CheckoutService } from './checkout.service';
+import { UserId } from '../auth/decorators/user-id/user-id.decorator';
 
 @Controller('checkout')
-export class CheckoutController {}
+export class CheckoutController {
+  constructor(private readonly checkoutService: CheckoutService) {}
+
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  checkout(@UserId() userId: string) {
+    return this.checkoutService.checkout(userId);
+  }
+}
--- apps/api-gateway/src/checkout/checkout.service.ts
+++ apps/api-gateway/src/checkout/checkout.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+
+import { MicroserviceError } from '../interfaces/microservice-error/microservice-error.interface';
 
 @Injectable()
-export class CheckoutService {}
+export class CheckoutService {
+  constructor(@Inject('PAYMENT_SERVICE') private paymentClient: ClientProxy) {}
+
+  private logger = new Logger(CheckoutService.name);
+
+  private handleError(error: MicroserviceError, action: string) {
+    this.logger.error(`Failed to ${action}`, error);
+
+    throw new InternalServerErrorException(`Failed to ${action}`);
+  }
+
+  async checkout(userId: string) {
+    try {
+      const stripeSessionUrl = await firstValueFrom<string>(
+        this.paymentClient.send({ cmd: 'checkout' }, userId),
+      );
+
+      return { stripeSessionUrl };
+    } catch (error) {
+      this.handleError(error as MicroserviceError, 'checkout');
+    }
+  }
+}

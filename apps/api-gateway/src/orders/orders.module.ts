adfac24a55894f701bd81ab6d59facea199d8c12
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-27 12:44:12 +0000

    feat(orders): Add asynchronous registration for Orders service

--- apps/api-gateway/src/orders/orders.module.ts
+++ apps/api-gateway/src/orders/orders.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 
 @Module({
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        imports: [ConfigModule],
+        name: 'ORDERS_SERVICE',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('MESSAGE_BROKER_URL') as string],
+            queue: 'orders_queue',
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })

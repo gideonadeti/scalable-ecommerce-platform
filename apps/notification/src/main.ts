8109cb63880fb4ba904f726c6b4502b0ccbc8dfd
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-27 15:58:52 +0000

    feat(notification): Implement microservice setup with RMQ

--- apps/notification/src/main.ts
+++ apps/notification/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { AsyncMicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+
 import { NotificationModule } from './notification.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NotificationModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<AsyncMicroserviceOptions>(
+    NotificationModule,
+    {
+      useFactory: (configService: ConfigService) => ({
+        transport: Transport.RMQ,
+        options: {
+          urls: [configService.get<string>('MESSAGE_BROKER_URL') as string],
+          queue: 'notification_queue',
+        },
+      }),
+      inject: [ConfigService],
+    },
+  );
+
+  await app.listen();
 }
+
 bootstrap();
